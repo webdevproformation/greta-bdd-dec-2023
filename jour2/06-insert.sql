301377aad24439141d4091772c04969cfe34dcd3
Malik H <malik.h@webdevpro.net>
2023-12-15 16:31:44 +0100

    requete update

--- jour2/06-insert.sql
+++ jour2/06-insert.sql
@@ -7,7 +7,7 @@ CREATE TABLE eleves (
 
 -- pour la colonne id => on ne la met pas dans les colonnes de la première parenthèse => id NULL => SQLITE qui va déterminer la valeur d'id 
 INSERT INTO eleves 
-( nom , age , dt_creation)
+( nom , age , dt_creation ) 
 VALUES
 (  "Alain" , 22 , "2023-12-15 15:46:10"  ),
 (  "Benois" , 12 , CURRENT_TIMESTAMP  ),
@@ -15,3 +15,20 @@ VALUES
 
 
 SELECT * FROM eleves ; 
+
+-- sur la table eleves ajouter les 4 lignes suivantes 
+
+-- Pierre / 40 / maintenant
+-- Zorro /  20 / le 1er janvier 2021 à 11h30
+-- Marc / 42 / le 31 décembre 2022 à 14h00
+-- Pierre / 60 / maintenant
+
+INSERT INTO eleves 
+( nom , age , dt_creation  )
+VALUES 
+("Pierre" , 40 , CURRENT_TIMESTAMP ), 
+("Zorro" , 20 , "2021-01-01 11:30:00" ), 
+("Marc" , 42 , "2022-12-31 14:00:00" ), 
+("Pierre" , 60 , CURRENT_TIMESTAMP ) ;
+
+SELECT * FROM eleves ;
---
+++ jour2/07-update.sql
@@ -0,0 +1,47 @@
+-- je souhaite modifier la valeur de l'age de Céline
+-- elle a dans la base de donnée 40 ans
+-- => 42 
+
+UPDATE eleves 
+SET age = 42 
+-- !!! attention si vous réalisez requête UPDATE SANS la clause WHERE 
+-- vous allez modifier l'age de toutes les lignes de la table eleves 
+
+UPDATE eleves 
+SET age = 42 
+WHERE nom = "Céline" ; 
+
+SELECT * FROM eleves ;
+
+UPDATE eleves 
+SET age = 65 
+WHERE nom = "Pierre" ;
+
+UPDATE eleves 
+SET age = 30
+WHERE id = 7 ; 
+
+--- il est possible de modifier plusieurs informations sur une ligne 
+-- modifier le nom et modifier l'age
+
+UPDATE eleves
+SET nom = "Benoit" , age = 15 
+WHERE id = 2 ;
+
+
+SELECT * FROM eleves ;
+
+-- modifier l'age de Pierre et Zorro 
+-- désormais ils ont 30 
+
+UPDATE eleves
+SET age = 30
+WHERE id = 4 OR id = 5 ;
+
+UPDATE eleves
+SET age = 30
+WHERE id BETWEEN 4 AND 5 ;
+
+UPDATE eleves
+SET age = 30
+WHERE id IN ( 4 , 5 ) ;
