86d67e7cdfb4f05180bbf196242f73614d40accf
Malik H <malik.h@webdevpro.net>
2023-12-18 16:05:32 +0100

    cas pratique

--- jour3/07-jointure.sql
+++ jour3/07-jointure.sql
@@ -19,9 +19,30 @@ on matiere.id = etudiant_matiere.id_matiere ;
 -- jointure entre plusieurs tables alias de Table 
 -- AS sur les tables
 
+-- relation manyToMany
 SELECT e.nom , m.nom 
 FROM etudiant AS e 
 JOIN etudiant_matiere AS em 
 ON e.id = em.id_etudiant 
 JOIN matiere  AS m 
 on m.id = em.id_matiere ; 
+
+-- cas pratique 
+-- oneToMany 
+-- on vous donne un fichier excel avec les données suivantes 
+
+id | nom client | nom salle | ville où est située la salle 
+1     Pierre       Verte       Paris
+2     Pierre       Jaune       Paris
+3     Alain        Verte       Paris
+4     Céline       Rose        Lyon
+5     Pierre       Rose        Lyon
+
+Question 1 => combien de concept === combien de table 
+Question 2 => quel est la relation entre les concepts ? tables ??? 
+
+Question 3 => Créer en SQL les tables avec des CREATE TABLE Clé primaire / clé secondaire
+
+Question 4 => INSERT de DOnnées (dans un certain ordre)
+
+
